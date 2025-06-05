048b5f903e275ed156a048b94e584fefd5a4e35a
JananiKala <jananikumaresan2004.r@gmail.com>
2025-06-05 15:06:53 +0530

    change

--- index.js
+++
@@ -1,9 +0,0 @@
-const upload=document.getElementById("upload");
-const profilepic=document.getElementById("profile-pic");
-upload.onchange=function(){
-    const reader=new FileReader();
-    reader.onload=function() {
-    profilepic.src=reader.result;
-};
-reader.eraderAsDataUrl(upload.file[0]);
-};
